e07f7f48e923fc033828751f80e9d112d8d69d34
Santiago Villaverde <santiago@passportglobal.com>
2026-08-26 17:07:43 -0300

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://vsantos77.github.io'
+    site: 'https://vsantos77.github.io',
+    base: '/personal-website',
 });
